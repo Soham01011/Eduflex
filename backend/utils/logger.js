8ecc5485b46e285ce6648d3dca04548920916a5e
Soham Dalvi <crafterking@tutanota.com>
2024-12-16 22:53:43 +0530

    utils added

---
+++ backend/utils/logger.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path  = require('path');
+const { format } = require('date-fns');
+
+const logDirectory = path.join(__dirname,'..', 'logs');
+
+// Create the logs directory if it doesn't exist
+if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
+}
+
+const getLogFileName = () => {
+    const dateStr = format(new Date(), 'yyyy-MM-dd');
+    return path.join(logDirectory, `${dateStr}.log`);
+};
+
+const logMessage = (message) => {
+    const logFile = getLogFileName();
+    const timestamp = new Date().toISOString();
+    const logEntry = `[${timestamp}] ${message}\n`;
+
+    fs.open(logFile, 'a', (err, fd) => {
+        if (err) {
+            throw err;
+        }
+
+        fs.appendFile(fd, logEntry, (err) => {
+            if (err) throw err;
+            fs.close(fd, (err) => {
+                if (err) throw err;
+            });
+        });
+    });
+};
+
+module.exports= {logMessage}
